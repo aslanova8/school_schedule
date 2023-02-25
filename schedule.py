b35bdfca7026c35c64710a457cc64ed9c947b889
aslanova8 <aslanova.polina@inbox.ru>
2023-02-25 16:59:05 +0300

    Dedublicate functions of loading input data

--- schedule.py
+++ schedule.py
@@ -1,3 +1,4 @@
+import tkinter
 import pandas as pd
 from tkinter import messagebox
 from tkinter import *
@@ -15,7 +16,7 @@ class Schedule(Frame):
     df_teachers = pd.DataFrame
     df_audiences = pd.DataFrame
     df_audiences_lessons = pd.DataFrame
-    df_calls = pd.DataFrame
+    df_rings = pd.DataFrame
     df_academic_plan = pd.DataFrame
 
     def __init__(self, parent):
@@ -52,73 +53,76 @@ class Schedule(Frame):
                                              relief=RAISED, borderwidth=1)
         frame_audiences_lessons.pack(fill=BOTH, expand=True)
 
-        frame_calls = LabelFrame(tab_files, text="Расписание звонков", relief=RAISED, borderwidth=1)
-        frame_calls.pack(fill=BOTH, expand=True)
+        frame_rings = LabelFrame(tab_files, text="Расписание звонков", relief=RAISED, borderwidth=1)
+        frame_rings.pack(fill=BOTH, expand=True)
 
         # Создание кнопок для вызова диалогового окна для загрузки
-        load_button_a_p = Button(frame_academic_plan, text="Загрузить", command=lambda: load_academic_plan(self))
+        load_button_a_p = Button(frame_academic_plan, text="Загрузить", command=lambda: load_df('academic_plan'))
         load_button_a_p.pack(side=RIGHT, padx=10, pady=10)
 
-        load_button_t = Button(frame_teachers, text="Загрузить", command=lambda: load_teachers(self))
+        load_button_t = Button(frame_teachers, text="Загрузить", command=lambda: load_df('teachers'))
         load_button_t.pack(side=RIGHT, padx=10, pady=10)
 
-        load_button_a = Button(frame_audiences, text="Загрузить", command=lambda: load_audiences(self))
+        load_button_a = Button(frame_audiences, text="Загрузить", command=lambda: load_df('audiences'))
         load_button_a.pack(side=RIGHT, padx=10, pady=10)
 
-        load_button_a_l = Button(frame_audiences_lessons, text="Загрузить",
-                                 command=lambda: load_audiences_lessons(self))
-        load_button_a_l.pack(side=RIGHT, padx=10, pady=10)
+        load_button_audiences_lessons = Button(frame_audiences_lessons, text="Загрузить",
+                                               command=lambda: load_df('audiences_lessons'))
+        load_button_audiences_lessons.pack(side=RIGHT, padx=10, pady=10)
 
-        load_button_c = Button(frame_calls, text="Загрузить", command=lambda: load_calls(self))
+        load_button_c = Button(frame_rings, text="Загрузить", command=lambda: load_df('rings'))
         load_button_c.pack(side=RIGHT, padx=10, pady=10)
 
-        # Функции для выгрузки сообщений по результату загрузки
-        # TODO: дедублицировать эти функции
-        def load_academic_plan(self):
-            Schedule.df_academic_plan = self.load_df()
-            if not Schedule.df_academic_plan.empty:
-                label_loaded_academic_plan = Label(frame_academic_plan, fg='Green',
-                                                   text=f'Загружен план обучения')
-                label_loaded_academic_plan.pack(side=LEFT, padx=10, pady=10)
+        # Сообщения по выгрузке данных
+        label_loaded_academic_plan = Label(frame_academic_plan, text=f'Загружен план обучения', fg='#f0f0f0')
+        label_loaded_academic_plan.pack(side=LEFT, padx=10, pady=10)
+        label_loaded_teachers = Label(frame_teachers, text=f'Загружена информация о учителях', fg='#f0f0f0')
+        label_loaded_teachers.pack(side=LEFT, padx=10, pady=10)
+        label_loaded_audiences = Label(frame_audiences, text=f'Загружена информация о аудиториях', fg='#f0f0f0')
+        label_loaded_audiences.pack(side=LEFT, padx=10, pady=10)
+        label_loaded_rings = Label(frame_rings, text=f'Загружена информация о звонках', fg='#f0f0f0')
+        label_loaded_rings.pack(side=LEFT, padx=10, pady=10)
+        label_loaded_audiences_lessons = Label(frame_audiences_lessons, text=f'Загружена информация о типах аудиторий',
+                                               fg='#f0f0f0')
+        label_loaded_audiences_lessons.pack(side=LEFT, padx=10, pady=10)
+        def load_df(df: str):
+            str_save = 'Schedule.df_'+df+' = pd.read_excel(fn)'
+            str_message = 'label_loaded_'+df+".configure(fg='Green')"
+            str_load = 'label_loaded_'+df+".configure(fg='#f0f0f0')"
 
-        def load_teachers(self):
-            Schedule.df_teachers = self.load_df()
-            if not Schedule.df_teachers.empty:
-                teacher_count = Schedule.df_teachers.shape[0]
-                label_loaded_teachers = Label(frame_teachers, fg='Green',
-                                              text=f'Загружены данные о {teacher_count} учителях')
-                label_loaded_teachers.pack(side=LEFT, padx=10, pady=10)
+            # TODO: Разобраться, почему без этого не работают лейблы
+            label_loaded_academic_plan.configure()
+            label_loaded_teachers.configure()
+            label_loaded_audiences.configure()
+            label_loaded_rings.configure()
+            label_loaded_audiences_lessons.configure()
 
-        def load_audiences(self):
-            Schedule.df_audiences = self.load_df()
-            if not Schedule.df_audiences.empty:
-                audiences_count = Schedule.df_audiences.shape[0]
-                label_loaded_audiences = Label(frame_audiences, fg='Green',
-                                               text=f'Загружены данные о {audiences_count} аудиоториях')
-                label_loaded_audiences.pack(side=LEFT, padx=10, pady=10)
-
-        def load_audiences_lessons(self):
-            Schedule.df_audiences_lessons = self.load_df()
-            if not Schedule.df_audiences_lessons.empty:
-                label_loaded_audiences_lessons = Label(frame_audiences_lessons, fg='Green',
-                                                       text=f'Загружены данные о типах аудиторий')
-                label_loaded_audiences_lessons.pack(side=LEFT, padx=10, pady=10)
-
-        def load_calls(self):
-            Schedule.df_calls = self.load_df()
-            if not Schedule.df_calls.empty:
-                label_loaded_calls = Label(frame_calls, fg='Green',
-                                           text=f'Загружены данные o звонках')
-                label_loaded_calls.pack(side=LEFT, padx=10, pady=10)
+            exec(str_load)
+            fn = filedialog.Open(self.parent, filetypes=[('Excel files', '.xlsx .xls')]).show()
+            # (("Template files", "*.tplate")
+            #  , ("HTML files", "*.html;*.htm")
+            #  , ("All files", "*.*")))
+            # TODO: Добавить другие типы файлов
+            if fn == '':
+                return
+            else:
+                exec(str_save)
+                exec(str_message)
 
         # Параметры расписания
 
         # Количество учебных дней в неделе
-        tab_parameters
+        label_days_in_week = Label(tab_parameters, text='Количество учебных дней в неделе')
+        label_days_in_week.pack(side=TOP, padx=10, pady=10)
         combo_days_in_week = Combobox(tab_parameters)
         combo_days_in_week['values'] = (4, 5, 6)
         combo_days_in_week.current(1)  # Вариант по умолчанию
         combo_days_in_week.pack(side=TOP, padx=10, pady=10)
+
+        # Вторая смена
+        second_shift = IntVar()
+        second_shift_checkbutton = ttk.Checkbutton(tab_parameters, text="Вторая смена", variable=second_shift)
+        second_shift_checkbutton.pack(side=TOP, padx=10, pady=10)
 
         # Кнопка загрузки параметров
         def load_parameters_def(self):
@@ -132,42 +136,33 @@ class Schedule(Frame):
         quit_button.pack(side=RIGHT, padx=10, pady=10)
 
         # Создание расписания
+        frame_table = LabelFrame(tab_schedule, text="Расписание", relief=RAISED, borderwidth=1)
+        frame_table.pack(fill=BOTH, expand=True)
         ga_button = Button(tab_schedule, text="Создать расписание",
-                           command=lambda: self.create_schedule(tab_files, tab_parameters, tab_schedule))
+                           command=lambda: self.create_schedule(tab_schedule, frame_table))
         ga_button.pack(side=BOTTOM, padx=10, pady=10)
         return
 
-    def load_df(self):
-        fn = filedialog.Open(self.parent, filetypes=[('Excel files', '.xlsx .xls')]).show()
-        # (("Template files", "*.tplate")
-        #  , ("HTML files", "*.html;*.htm")
-        #  , ("All files", "*.*")))
-        # TODO: Добавить другие типы файлов
-        if fn == '':
-            return
-        else:
-            df = pd.read_excel(fn)
-            return df
 
     def quit(self):
         if messagebox.askyesno("Выйти", "Закрыть программу?"):
             self.parent.destroy()
 
-    def create_schedule(self, tab_files, tab_parameters, tab_schedule):
+    def create_schedule(self, tab_schedule, frame_schedule):
         if Schedule.df_teachers.empty or Schedule.df_audiences.empty or Schedule.df_academic_plan.empty or \
-                Schedule.df_calls.empty or Schedule.df_audiences_lessons.empty:
+                Schedule.df_rings.empty or Schedule.df_audiences_lessons.empty:
             if messagebox.showinfo("Загрузить", "Введите все начальные данные"):
                 return
-        first_schedule = ga.get_empty_schedule(Schedule.df_calls, Schedule.number_of_days_in_week)
+        first_schedule = ga.get_empty_schedule(Schedule.df_rings, Schedule.number_of_days_in_week)
         first_schedule = ga.create_first_population(first_schedule, Schedule.df_academic_plan, Schedule.df_teachers,
                                                     Schedule.df_audiences_lessons, Schedule.df_audiences)
-        temp = 0
         # TODO: прописать остальные этапы генетического алгоритма
-        self.show_schedule(tab_schedule, first_schedule)
+        self.show_schedule(tab_schedule, frame_schedule, first_schedule)
 
     @staticmethod
-    def show_schedule(tab, schedule: dict):
+    def show_schedule(tab, frame, schedule: dict):
 
+        # Преобразование расписания
         distinct_classes = sorted(Schedule.df_academic_plan['class'].unique())
         data = [['' for __ in range(len(distinct_classes))] for _ in range(len(schedule))]
         interval_num = 0
@@ -184,10 +179,10 @@ class Schedule(Frame):
             table.append((interval,) + tuple(data[interval_num]))
             interval_num += 1
 
+        # Создание таблицы в Tkinter
         columns = ('',) + tuple(distinct_classes)
 
-        tree = ttk.Treeview(tab, columns=columns, show="headings")
-        tree.pack(fill=BOTH, expand=1)
+        tree = ttk.Treeview(frame, columns=columns, show="headings")
 
         tree.heading("", text="", anchor=W)
         for i in distinct_classes:
@@ -196,8 +191,20 @@ class Schedule(Frame):
         for interval in table:
             tree.insert("", END, values=interval)
 
-        def save_schedule_def(schedule):
-            df = pd.DataFrame(schedule)
+        # Добавление вертикальной прокрутки
+        scrollbar_v = ttk.Scrollbar(frame, orient=VERTICAL, command=tree.yview)
+        tree.configure(yscrollcommand=scrollbar_v.set)
+        scrollbar_v.pack(side=RIGHT, fill=Y)
+
+        # Добавление горизонтальной прокрутки
+        scrollbar_h = ttk.Scrollbar(frame, orient=HORIZONTAL, command=tree.xview)
+        tree.configure(xscrollcommand=scrollbar_h.set)
+        scrollbar_h.pack(side=BOTTOM, fill=X)
+
+        tree.pack(fill=BOTH, expand=1)
+
+        def save_schedule_def(schedule_table):
+            df = pd.DataFrame(schedule_table)
             df.columns = [''] + distinct_classes
             # TODO: Добавить другие типы файлов
             df.to_excel('./schedule.xlsx', sheet_name='Budgets', index=False)
@@ -212,7 +219,7 @@ def main():
     # Создание корневого окна
     root = Tk()
     root.geometry("800x500+0+0")
-    app = Schedule(root)
+    Schedule(root)
     root.mainloop()
 
 
